301739dff06b27f3c0646901a2cc25ebb834a945
Heldson L Silva <heldsonluiz@gmail.com>
2023-06-06 22:40:06 -0300

    :construction: Estrutura visual

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,33 @@
+import { Summary } from "../../components";
+import { Header } from "../../components/Header";
+import { SearchForm } from "./SearchForm";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
+
+export function Transactions() {
+  return (
+    <div>
+      <Header/>
+      <Summary/>
+
+      <TransactionsContainer>
+        <SearchForm></SearchForm>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td><PriceHighlight variant="income">R$ 12.000,00</PriceHighlight></td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td><PriceHighlight variant="outcome">-R$ 50,00</PriceHighlight></td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  )
+}
